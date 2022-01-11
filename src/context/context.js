07a6ae5c602be560a82bbe6f06854074fb243165
quangTD <tsquang398@gmail.com>
2022-01-11 16:17:05 +0700

    :sparkles: [24 GITHUB SEARCH USERS] feat 253. setup contextAPI

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect, useContext } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = "https://api.github.com";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [githubRepos, setGithubRepos] = useState(mockRepos);
+  const [githubFollowers, setGithubFollowers] = useState(mockFollowers);
+  return (
+    <AppContext.Provider value={{ githubUser, githubRepos, githubFollowers }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import { Info, Repos, User, Search, Navbar } from '../components';
-import loadingImage from '../images/preloader.gif';
-import { GithubContext } from '../context/context';
+import React from "react";
+import { Info, Repos, User, Search, Navbar } from "../components";
+import loadingImage from "../images/preloader.gif";
+import { GithubContext, useGlobalContext } from "../context/context";
 const Dashboard = () => {
+  const data = useGlobalContext();
+  console.log("🚀TCL: ~ file: Dashboard.js ~ line 7 ~ Dashboard ~ data", data);
   return (
     <main>
+      <Navbar />
+      <Info />
       <h2>Dashboard Page</h2>
     </main>
   );
