6a593861b4ea3e15f3d1023630e920ce3474bc17
quangTD <tsquang398@gmail.com>
2022-01-11 10:35:43 +0700

    :sparkles: [24 GITHUB SEARCH USERS] implement error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 const Error = () => {
-  return <h2>error page</h2>;
+  return (
+    <Wrapper>
+      <div>
+        <h1>404</h1>
+        <h3>Sorry, The Page You Tried Cannot Be Found</h3>
+        <Link to={"/"} className='btn'>
+          Back Home
+        </Link>
+      </div>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.section`
   min-height: 100vh;
+  //& using grid
   display: grid;
   place-items: center;
+  //& using flex
+  /* display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column; */
   background: var(--clr-primary-10);
   text-align: center;
   h1 {
