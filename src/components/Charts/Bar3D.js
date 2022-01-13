883ecc3d5d347e5046d9c29990b4721153a30bac
quangTD <tsquang398@gmail.com>
2022-01-13 15:46:09 +0700

    :sparkles: [24 GITHUB SEARCH USERS] setup column and bar chart

--- src/components/Charts/Bar3D.js
+++ src/components/Charts/Bar3D.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Chart from "fusioncharts/fusioncharts.charts";
+// import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
 
-const Bar3D = () => {
-  return <div>chart</div>;
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+const Bar3D = ({ data }) => {
+  const chartConfigs = {
+    type: "bar2d",
+    width: "100%",
+    height: "400",
+    dataFormat: "json",
+    dataSource: {
+      chart: {
+        caption: "Most Forked",
+        theme: "fusion",
+        // theme: "candy",
+        decimals: 0, // display decinal or not [ 0 or 1 (default) ]
+        doughnutRadius: "45%",
+        // paletteColors: "green" // // repeated color
+        // numberSuffix: "%",
+        showPercentValues: 0, //show value instead of percentage
+        xAxisName: "Repos",
+        yAxisName: "Forked",
+      },
+      // Chart Data
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
 export default Bar3D;
--- src/components/Charts/Column3D.js
+++ src/components/Charts/Column3D.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Chart from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+// import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
 
-const Column3D = () => {
-  return <div>chart</div>;
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+const Column3D = ({ data }) => {
+  const chartConfigs = {
+    type: "column2d",
+    width: "100%",
+    height: "400",
+    dataFormat: "json",
+    dataSource: {
+      chart: {
+        caption: "Most Popular",
+        // theme: "fusion",
+        // decimals: 0, // display decinal or not [ 0 or 1 (default) ]
+        xAxisName: "Repos",
+        yAxisName: "Stars",
+        xAxisNameFontSize: "16px",
+        yAxisNameFontSize: "16px",
+      },
+      // Chart Data
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
 export default Column3D;
